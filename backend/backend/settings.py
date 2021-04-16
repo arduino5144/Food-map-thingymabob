1b8c204a78975992fb767aab2543f23ca37ca65c
superMinecraftL <Leonard@tussilago.eu>
2021-04-16 08:27:55 +0700

    WIP

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodmap',
+        'USER': os.environ.get('PGUSER', 'user'),
+        'PASSWORD': os.environ.get('PGPASS', 'password123'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
